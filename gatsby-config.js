be2196127ff5af196fc2e6dbd0b003120356d94f
digital-est <digital.est62@gmail.com>
2020-02-27 14:00:51 +0800

    Change site title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'DIGITAL EST',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'DIGITAL EST is a healthcare solution provider',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
